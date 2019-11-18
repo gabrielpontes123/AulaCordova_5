0b0da2116d8005662e485655fe235b922fc84eca
gabrielpontes123 <gabrielpontesfernandes@gmail.com>
2019-11-18 20:02:45 -0300

    AulaCordova_5

---
+++ www/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA5WIGIQ6-t025dA_NNSi09TGLiVrrGM8s",
+    authDomain: "aula-firebase1.firebaseapp.com",
+    databaseURL: "https://aula-firebase1.firebaseio.com",
+    projectId: "aula-firebase1",
+    storageBucket: "aula-firebase1.appspot.com",
+    messagingSenderId: "961755690831",
+    appId: "1:961755690831:web:ea3c191f9a3aadb2fab73a",
+    measurementId: "G-ES9EZ0K2EJ"
+  };
